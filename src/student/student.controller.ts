e9f6a3fca707e3d4f54b26b02ab4b228fccafcd7
BrianH <brianhwang29@gmail.com>
2021-06-10 13:00:01 +0900

    modified with connecting service

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,29 +1,33 @@
 import { Controller, Get, Post, Put, Param, Body } from "@nestjs/common";
 import {CreateStudentDto, UpdateStudentDto, FindStudentResponseDto, StudentResponseDto} from './dto/student.dto'
+import { StudentService } from "./student.service";
 
 @Controller('students')
 export class StudentController{
-@Get()
-getStudents(): FindStudentResponseDto[]{
-    return `All Students`
-}
 
-@Get('/:studentId')
-getStudentById(@Param('studentId') studentId: string) : FindStudentResponseDto{
-    return `get student with id of ${studentId}`
-}
+  constructor(private readonly studentService: StudentService){}
 
-@Post()
-createStudent(@Body() body: CreateStudentDto): StudentResponseDto{
-    return `create student with the following data ${JSON.stringify(body)}`
-}
+  @Get()
+  getStudents(): FindStudentResponseDto[]{
+    return this.studentService.getStudents();
+  }
 
-@Put('/:studentId')
-updateStudent(
-    @Param('studentId') studentId: string,
-    @Body() body : UpdateStudentDto   
-): StudentResponseDto{
-    console.log(body);
-    return `update student with id of ${studentId} data of ${JSON.stringify(body)}`
-}
+  @Get('/:studentId')
+  getStudentById(@Param('studentId') studentId: string) : FindStudentResponseDto{
+    return this.studentService.getStudentById(studentId); 
+  }
+
+  @Post()
+  createStudent(@Body() body: CreateStudentDto): StudentResponseDto{
+    return this.studentService.createStudent(body); 
+  }
+
+  @Put('/:studentId')
+  updateStudent(
+      @Param('studentId') studentId: string,
+      @Body() body : UpdateStudentDto   
+  ): StudentResponseDto{
+      console.log(body);
+      return this.studentService.updateStudent(studentId, body); 
+  }
 }
