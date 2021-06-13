4a0aabe88bdbdfc5d6ef12d526a0f751aeb7d327
brian <brianhwang29@gmail.com>
2021-06-13 09:38:24 +0900

    implement studentModule, teacherModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,8 @@
 import { Module } from '@nestjs/common';
-import { StudentTeacherController } from 'src/teacher/student.controller';
-import { StudentController } from "../student/student.controller";
-import { TeacherController } from "../teacher/teacher.controller";
-import { StudentService } from '../student/student.service';
-import { TeacherService } from 'src/teacher/teacher.service';
+import { StudentModule } from 'src/student/student.module';
+import { TeacherModule } from 'src/teacher/teacher.module';
 
 @Module({
-  imports: [],
-  controllers: [StudentController, TeacherController, StudentTeacherController],
-  providers: [StudentService, TeacherService]
+  imports: [TeacherModule, StudentModule]
 })
 export class AppModule {}
